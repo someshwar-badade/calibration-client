635565905d7d4207ff95ebe0071847963461e90b
someshwar-badade <92147272+someshwar-badade@users.noreply.github.com>
2022-03-30 22:11:19 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,11 @@
+def buidApp(){
+  echo "building the application..."
+}
+
+def testApp(){
+  echo "testing the application..."
+}
+
+def deployApp(){
+  echo "deploying..... version=${params.VERSION}"
+}
